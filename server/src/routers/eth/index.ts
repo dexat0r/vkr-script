8ec2186d395ea7d530f768393bb667145cf2d150
dexat0r <nikit-balak@yandex.ru>
2022-05-30 00:42:55 +0300

    Working api to get money transfers beetwen wallets

--- server/src/routers/eth/index.ts
+++ server/src/routers/eth/index.ts
@@ -1,10 +1,9 @@
-import { Request, Response, Router } from 'express';
-import createHttpError from 'http-errors';
-import Web3Client from '../../scripts';
+import { Request, Response, Router } from "express";
+import createHttpError from "http-errors";
+import Web3Client from "../../scripts";
 
 class ETHController {
-    
-    web3: Web3Client
+    web3: Web3Client;
 
     constructor() {
         this.web3 = new Web3Client();
@@ -12,12 +11,8 @@ class ETHController {
 
     getUserTxs = async (req: Request, res: Response) => {
         try {
-            let {
-                address, 
-                from,
-                to
-            } = req.body;
-    
+            let { address, from, to } = req.body;
+
             if (!address && !from && !to) {
                 throw new createHttpError.BadRequest();
             }
@@ -26,12 +21,22 @@ class ETHController {
                 throw new createHttpError.BadRequest();
             }
 
-            if (to > Date.now()) to = Date.now()
+            if (to > Date.now()) to = Date.now();
 
-            const [uFrom, uTo] = await this.web3.calculateBlockNumbers(Number(from),Number(to));
-    
+            const [uFrom, uTo] = await this.web3.calculateBlockNumbers(
+                Number(from),
+                Number(to)
+            );
+
             try {
-                const txs = await this.web3.getAllUserTransactions(uFrom, uTo, address);
+                const allTxs = await this.web3.getAllTransactionsFromBlocks(
+                    uFrom,
+                    uTo
+                );
+                const txs = await this.web3.getAllUserTransactions(
+                    allTxs,
+                    address
+                );
                 res.status(200).json(txs);
             } catch (error: any) {
                 throw new createHttpError.ServiceUnavailable(error);
@@ -40,16 +45,53 @@ class ETHController {
             console.log(error);
             res.status(error.status).json(error.message);
         }
-    }
+    };
+
+    findPair = async (req: Request, res: Response) => {
+        try {
+            let {
+                from,
+                to,
+                fromTime,
+                toTime,
+            }: { from: string; to: string; fromTime: number; toTime: number } =
+                req.body;
+
+            if (!from && !to && !fromTime && !toTime) {
+                throw new createHttpError.BadRequest();
+            }
+            if (toTime > Date.now()) toTime = Date.now();
+
+            const [uFrom, uTo] = await this.web3.calculateBlockNumbers(
+                Number(fromTime),
+                Number(toTime)
+            );
+            const txs = await this.web3.getAllTransactionsFromBlocks(
+                uFrom,
+                uTo
+            );
+
+            const result = await this.web3.findPairTxs(
+                this.web3.web3.utils.toChecksumAddress(from),
+                this.web3.web3.utils.toChecksumAddress(to),
+                txs
+            );
+            return res.status(200).json(result);
+        } catch (error: any) {
+            console.log(error);
+            res.status(error.status).json(error.message);
+        }
+    };
 }
 
 export default class ETHRouter {
-    path = "/eth"
+    path = "/eth";
     router = Router();
 
     constructor() {
         const eth = new ETHController();
 
-        this.router.post('/', eth.getUserTxs);
+        this.router.post("/", eth.getUserTxs);
+        this.router.post("/pair", eth.findPair);
     }
 }
--- server/src/scripts/index.ts
+++ server/src/scripts/index.ts
@@ -1,89 +1,188 @@
 import Web3 from "web3";
-import * as dotenv from 'dotenv';
-import { Transaction } from "web3-core"
-import ethBlock from 'ethereum-block-by-date';
+import * as dotenv from "dotenv";
+import { Transaction } from "web3-core";
+import ethBlock from "ethereum-block-by-date";
 
 dotenv.config();
+interface TxGraph {
+    [key: string]: {
+        fromTx: Transaction[];
+        toTx: Transaction[];
+        visited: boolean;
+    };
+}
+
+interface txDescr {
+    from: string,
+    to: string
+    amount: string,
+    blockNumber: number
+}
 
 export default class Web3Client {
-    
     web3: Web3;
     updateTime = 5;
-    
+
     constructor() {
         const apiKey = process.env.INFURA_KEY;
         this.web3 = new Web3(`http://localhost:8545`);
         this.web3.eth.getChainId().then((value) => {
-            console.log(`Web3 client initialized at chain with id ${value.toLocaleString()}`)
-        })
+            console.log(
+                `Web3 client initialized at chain with id ${value.toLocaleString()}`
+            );
+        });
     }
 
-    async getAllUserTransactions(fromBlock: number, toBlock: number, _address: string) {
-        const address = this.web3.utils.toChecksumAddress(_address)
+    async getAllUserTransactions(
+        _txs: Transaction[],
+        _address: string
+    ): Promise<Transaction[]> {
+        const address = this.web3.utils.toChecksumAddress(_address);
         try {
-            console.log(`Getting transactions from address ${address}. From: ${fromBlock}. To: ${toBlock}`);
-            let txs: Transaction[] = []
+            let txs: Transaction[] = [];
+            for (let j = 0; j < _txs.length; j++) {
+                if (_txs[j].from == address || _txs[j].to == address) {
+                    txs.push(_txs[j]);
+                }
+            }
+            return txs;
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async getAllTransactionsFromBlocks(
+        fromBlock: number,
+        toBlock: number
+    ): Promise<Transaction[]> {
+        try {
+            console.log(
+                `Getting transactions... From: ${fromBlock}. To: ${toBlock}`
+            );
+            let txs: Transaction[] = [];
             for (let i = fromBlock; i < toBlock; i++) {
                 const block = await this.web3.eth.getBlock(i);
-                console.log(block.number, block.transactions.length);
                 if (block != null && block.transactions != null) {
                     for (let j = 0; j < block.transactions.length; j++) {
-                        const tx = await this.web3.eth.getTransaction(block.transactions[j]);
-                        if (tx.from == address || tx.to == address) {
-                            txs.push(tx);
-                        }
+                        const tx = await this.web3.eth.getTransaction(
+                            block.transactions[j]
+                        );
+                        txs.push(tx);
                     }
                 }
             }
             return txs;
         } catch (error) {
-            console.log(error)
+            console.log(error);
             return [];
         }
     }
 
-    async calculateBlockNumbers(from: number, to: number): Promise<Array<number>> {
+    async calculateBlockNumbers(
+        from: number,
+        to: number
+    ): Promise<Array<number>> {
         const blocks: number[] = [];
         const eth = new ethBlock(this.web3);
-        blocks.push(
-            (await eth.getDate(from)).block
-        );
-        blocks.push(
-            (await eth.getDate(to)).block
-        );
+        blocks.push((await eth.getDate(from)).block);
+        blocks.push((await eth.getDate(to)).block);
         return blocks;
     }
 
-    // private async getClosestBlock(timestamp: number) {
-    //     const currentTime = Date.now();
-    //     const currentBlock = await this.web3.eth.getBlockNumber();
-    //     const tryBlock = await this.web3.eth.getBlock(currentBlock - Math.floor((Math.abs(currentTime - timestamp) / (this.updateTime)) / 1000));
-    //     let rotation = 0;
-    //     if (Number(tryBlock.timestamp) * 1000 > timestamp) {
-    //         rotation = -1;
-    //     } else {
-    //         rotation = 1;
-    //     }
-    //     let previousBlock = tryBlock.number;
-    //     while(true) {
-    //         const newTryBlock = await this.web3.eth.getBlock(previousBlock + rotation);
-    //         previousBlock = newTryBlock.number;
-    //         console.log(previousBlock, Number((await this.web3.eth.getBlock(previousBlock)).timestamp) * 1000, timestamp);
+    async findPairTxs(from: string, to: string, _txs: Transaction[]) {
+        try {
+            const graph = this.createGraphView(_txs);
+            const transferPath: txDescr[] = [];
+            const dfc = (currentNode: string, blockNumber: number) => {
+                const txsToCheck = graph[currentNode];
+                console.log(
+                    `DEBUG: current node: ${currentNode}, fromTxs: ${txsToCheck.fromTx}, toTxs: ${txsToCheck.toTx}`
+                );
 
-    //         if (Number(newTryBlock.timestamp) * 1000 <= timestamp && rotation < 0) {
-    //             break;
-    //         }
-    //         if (Number(newTryBlock.timestamp) * 1000 >= timestamp && rotation > 0) {
-    //             break;
-    //         }
-    //     }
-    //     return previousBlock
-    // }
+                if (!txsToCheck) {
+                    console.log(`DEBUG: No txsToCheck`);
+                    return false;
+                }
 
-    
+                if (txsToCheck.visited) {
+                    console.log(`DEBUG: already visited ${txsToCheck.visited}`);
+                    return false;
+                }
 
+                if (currentNode == to) {
+                    console.log(`DEBUG: node found ${currentNode}`);
+                    return true;
+                }
 
+                graph[currentNode].visited = true;
+                for (let i = 0; i < txsToCheck.fromTx.length; i++) {
+                    if (
+                        (txsToCheck.fromTx[i].blockNumber as number) >= blockNumber && // check if tx of the node could be after our previous tx
+                        txsToCheck.fromTx[i].to && //check if tx is not contract creation 
+                        graph[txsToCheck.fromTx[i].to as string] //check if tx has graph view
+                    ) {
+                        console.log(
+                            `DEBUG: graph of checking node`,
+                            graph[txsToCheck.fromTx[i].to as string]
+                        );
+                        if (!graph[txsToCheck.fromTx[i].to as string].visited) {
+                            let result = dfc(
+                                txsToCheck.fromTx[i].to as string,
+                                txsToCheck.fromTx[i].blockNumber as number
+                            );
+                            if (result) {
+                                const txDescr: txDescr = {
+                                    from: txsToCheck.fromTx[i].from,
+                                    to: (txsToCheck.fromTx[i].to as string),
+                                    blockNumber: txsToCheck.fromTx[i].blockNumber as number,
+                                    amount: txsToCheck.fromTx[i].value
+                                }
+                                transferPath.push(txDescr);
+                                return true;
+                            }
+                        }
+                    } else {
+                        console.log(`DEBUG: Leaf tx`);
+                    }
+                }
+            };
 
+            const result = dfc(from, 0);
+            if (result) {
+                console.log(transferPath);
+                return transferPath.reverse();
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
 
-
-}   
+    private createGraphView(txs: Transaction[]) {
+        const graph: TxGraph = {};
+        for (let i = 0; i < txs.length; i++) {
+            if (!graph[txs[i].from]) {
+                graph[txs[i].from] = {
+                    fromTx: [],
+                    toTx: [],
+                    visited: false,
+                };
+            }
+            graph[txs[i].from].fromTx.push(txs[i]);
+            if (txs[i].to) {
+                if (!graph[txs[i].to as string]) {
+                    graph[txs[i].to as string] = {
+                        fromTx: [],
+                        toTx: [],
+                        visited: false,
+                    };
+                }
+                graph[txs[i].to as string].toTx.push(txs[i]);
+            }
+        }
+        return graph;
+    }
+}
